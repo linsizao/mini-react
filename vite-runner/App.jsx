4be9f14abe754fc5deb5e20b6b66475f081dff78
linsizao <linsizao@163.com>
2024-03-25 23:21:07 +0800

    feat: 支持函数组件 & 无空闲时间的处理

--- vite-runner/App.jsx
+++ vite-runner/App.jsx
@@ -3,11 +3,33 @@ import React from './core/React.js'
 // const App = React.createElement('div', { id: 'app', }, 'react')
 // const App = <div>react</div>
 
-const App = <div>
-  <div>
-    111
-    <div>div-111</div>
-  </div>
-</div>
+// const App = <div>
+//   <div>
+//     1
+//     <div>1-1
+//       <div>div-111</div>
+//       <div>div-111</div>
+//       <div>div-111</div>
+//     </div>
+//   </div>
+// </div>
+
+// const App = () => <div>react</div>
+
+
+function Counter ({num}) {
+  return <div>count: {num}</div>
+}
+
+function CountConainer () {
+  return <Counter></Counter>
+
+}
+
+const App = () => (<div>
+  react
+  <Counter num={10}></Counter>
+  <Counter num={20}></Counter>
+</div>)
 
 export default App
--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -13,25 +13,32 @@ function createElement (type, props, ...children) {
     type,
     props: {
       ...props,
-      children: children.map((child) =>
-        typeof child === 'string' ? createTextNode(child) : child)
+      children: children.map((child) => {
+        console.log('child', child)
+        const isTextNode = ['string', 'number'].includes(typeof child)
+        return isTextNode ? createTextNode(child) : child
+      })
+
+
     }
   }
 }
 
 function render (el, container) {
-
-  console.log('render');
   nextWorkOfUnit = {
     dom: container,
     props: {
       children: [el]
     }
   }
+
+  root = nextWorkOfUnit
 }
 
 // 下一个工作单元
 let nextWorkOfUnit = null
+// 根节点
+let root = null
 
 // 工作循环
 function workLoop (deadline) {
@@ -43,7 +50,30 @@ function workLoop (deadline) {
     shouldYield = deadline.timeRemaining() < 1
   }
 
+  if (!nextWorkOfUnit && root) {
+    commitRoot()
+  }
+
   requestIdleCallback(workLoop)
+}
+
+function commitRoot () {
+  commitWork(root.child)
+}
+
+function commitWork (fiber) {
+  if (!fiber) return
+
+  let fiberParent = fiber.parent
+  while (!fiberParent.dom) {
+    fiberParent = fiberParent.parent
+  }
+  fiber.dom && fiberParent.dom.append(fiber.dom)
+
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+
+  root = null
 }
 
 function createDom (type) {
@@ -60,8 +90,8 @@ function updateProps (dom, props) {
   })
 }
 
-function initChilder (fiber) {
-  const children = fiber.props.children
+function initChilder (fiber, children) {
+  // const children = fiber.props.children
   let prevChild = null
   children.forEach((child, index) => {
     const newFiber = {
@@ -81,25 +111,41 @@ function initChilder (fiber) {
   })
 }
 
-// 处理工作单元
-function performWorkOfUnit (fiber) {
-  console.log('fiber', fiber)
+function isFunction (v) {
+  return typeof v === 'function'
+}
+
+function updateFunctioComponent (fiber) {
+  const children = [fiber.type(fiber.props)]
+  initChilder(fiber, children)
+}
+
+function updateHookComponent (fiber) {
   if (!fiber.dom) {
     const dom = fiber.dom = createDom(fiber.type)
-    fiber.parent.dom.append(dom)
+    // fiber.parent.dom.append(dom)
 
     if (fiber.props) updateProps(dom, fiber.props)
   }
 
-  initChilder(fiber)
+  const children = fiber.props.children
+  initChilder(fiber, children)
+
+}
+
+// 处理工作单元
+function performWorkOfUnit (fiber) {
+  console.log('fiber', fiber)
+  const isFunctioComponent = isFunction(fiber.type)
+  isFunctioComponent ? updateFunctioComponent(fiber) : updateHookComponent(fiber)
 
   if (fiber.child) return fiber.child
-  if (fiber.sibling) return fiber.sibling
+  // if (fiber.sibling) return fiber.sibling
 
-  let parent = fiber.parent
-  while (parent) {
-    if (parent.sibling) return parent.sibling
-    parent = parent.parent
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling
+    nextFiber = nextFiber.parent
   }
 }
 
